985b67c27f2108bb24365732cd2950dd87bc3436
Juraj Huska <jhuska@redhat.com>
2012-07-23 17:11:16 +0200

    Autocomplete test page from showcase rewritten with component objects

--- showcase-ftest/src/test/java/org/richfaces/tests/showcase/autocomplete/TestModes.java
+++ showcase-ftest/src/test/java/org/richfaces/tests/showcase/autocomplete/TestModes.java
@@ -21,106 +21,119 @@
  *******************************************************************************/
 package org.richfaces.tests.showcase.autocomplete;
 
-import static org.jboss.arquillian.ajocado.Graphene.guardXhr;
-import static org.jboss.arquillian.ajocado.locator.LocatorFactory.jq;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertNull;
 
-import java.awt.event.KeyEvent;
+import java.util.List;
 
-import org.jboss.arquillian.ajocado.dom.Event;
-import org.jboss.arquillian.ajocado.locator.JQueryLocator;
-import org.richfaces.tests.showcase.AbstractGrapheneTest;
+import org.jboss.arquillian.graphene.spi.components.autocomplete.Suggestion;
+import org.jboss.arquillian.graphene.spi.components.scrolling.ScrollingType;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.tests.components.autocomplete.AutocompleteComponentImpl;
+import org.richfaces.tests.components.autocomplete.TextSuggestionParser;
+import org.richfaces.tests.showcase.AbstractWebDriverTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
  * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
  * @version $Revision$
  */
-public class TestModes extends AbstractGrapheneTest {
+public class TestModes extends AbstractWebDriverTest {
 
     /* **************************************************************************************
      * Locators**************************************************************************************
      */
 
-    private JQueryLocator minCharInput = jq("input[type=text]:eq(0)");
-    private JQueryLocator multipleSelectionInput = jq("input[type=text]:eq(1)");
-    private JQueryLocator selectFirstFalseInput = jq("input[type=text]:eq(2)");
-    private JQueryLocator selection = jq("div.rf-au-itm");
+    @FindBy(css = "form:nth-of-type(1)")
+    private AutocompleteComponentImpl<String> autocompleteMinChar;
+
+    @FindBy(css = "form:nth-of-type(2)")
+    private AutocompleteComponentImpl<String> autocompleteMultipleSelections;
+
+    @FindBy(css = "form:nth-of-type(3)")
+    private AutocompleteComponentImpl<String> autocompleteSelectFirstFalse;
 
     /* **************************************************************************************
      * Tests**************************************************************************************
      */
 
-    @Test
+    @BeforeMethod
+    public void setParserToAutocompletes() {
+        TextSuggestionParser parser = new TextSuggestionParser();
+
+        autocompleteMinChar.setSuggestionParser(parser);
+        autocompleteMultipleSelections.setSuggestionParser(parser);
+        autocompleteSelectFirstFalse.setSuggestionParser(parser);
+    }
+
+    // @Test
     public void testMinCharInput() {
+        String expected = "California";
 
-        char key = 'a';
-        selenium.keyPress(minCharInput, 'a');
+        List<Suggestion<String>> suggestions = autocompleteMinChar.typeAndReturn("C");
+        assertNull(suggestions, "There should be no suggestions!");
 
-        assertFalse(selenium.isElementPresent(selection),
-            "The selection should not be visible, since there is only one char!");
+        suggestions = autocompleteMinChar.typeAndReturn("a");
+        assertEquals(suggestions.size(), 1, "There should be one suggestion!");
+        Suggestion<String> sugg = suggestions.get(0);
+        assertEquals((suggestions.get(0).getValue()), expected, "The expected suggestion should be: " + expected);
 
-        String keys = "ar";
-        selenium.focus(minCharInput);
-        selenium.type(minCharInput, keys);
-        guardXhr(selenium).fireEvent(minCharInput, Event.KEYPRESS);
+        autocompleteMinChar.autocompleteWithSuggestion(sugg);
+        String actual = autocompleteMinChar.getFirstInputValue();
+        assertEquals(actual, expected, "The suggestion was not selected correctly!");
+    }
 
-        assertTrue(selenium.isVisible(selection), "The selection should be visible, since there are two chars!");
-
-        String actualArizona = selenium.getText(jq(selection.getRawLocator() + ":eq(0)"));
-        assertEquals(actualArizona, "Arizona", "The provided option should be Arizona");
-
-        String actualArkansas = selenium.getText(jq(selection.getRawLocator() + ":eq(1)"));
-        assertEquals(actualArkansas, "Arkansas", "The provided option should be Arkansas");
-
+    @Test(groups = "4.Future")
+    public void testMultipleSelectionInputSelectByMouse() {
+        checkMultipleAutocomplete(ScrollingType.BY_MOUSE);
     }
 
     @Test
-    public void testMultipleSelectionInput() {
+    public void testMultipleSelectionInputSelectByKeys() {
+        checkMultipleAutocomplete(ScrollingType.BY_KEYS);
+    }
 
-        char key = 'a';
-        selenium.focus(multipleSelectionInput);
-        guardXhr(selenium).keyPress(multipleSelectionInput, key);
+    private void checkMultipleAutocomplete(ScrollingType scrollingType) {
+        String expected = "California";
 
-        assertTrue(selenium.isVisible(selection),
-            "The selection should be visible, since there is correct starting char!");
+        autocompleteMultipleSelections.type("Ca");
+        Suggestion<String> suggestion = autocompleteMultipleSelections.getFirstSuggestion();
+        assertEquals(suggestion.getValue(), expected, "The available suggestion is wrong!");
 
-        selenium.keyPressNative(KeyEvent.VK_ENTER);
+        autocompleteMultipleSelections.autocompleteWithSuggestion(suggestion);
 
-        key = ' ';
-        selenium.keyPress(multipleSelectionInput, key);
+        autocompleteMultipleSelections.type(" ");
+        autocompleteMultipleSelections.type("Was");
 
-        key = 'w';
+        expected += " Washington";
+        suggestion = autocompleteMultipleSelections.getFirstSuggestion();
+        autocompleteMultipleSelections.autocompleteWithSuggestion(suggestion, scrollingType);
 
-        selenium.focus(multipleSelectionInput);
-        guardXhr(selenium).keyPress(multipleSelectionInput, key);
-
-        assertTrue(selenium.isVisible(selection),
-            "The selection should be visible, since there is correct starting char!");
-
-        selenium.keyPressNative(KeyEvent.VK_ENTER);
-
-        String actualContentOfInput = selenium.getValue(multipleSelectionInput);
-        assertEquals(actualContentOfInput, "Alabama Washington", "The input should contain something else!");
+        assertEquals(autocompleteMultipleSelections.getInputValue(), expected, "The input shoul now contain diferent values!");
     }
 
     @Test
     public void testSelectFirstFalseInput() {
+        String expected = "Ca";
 
-        char key = 'a';
-        selenium.focus(selectFirstFalseInput);
-        guardXhr(selenium).keyPress(selectFirstFalseInput, key);
+        autocompleteSelectFirstFalse.type(expected);
 
-        assertTrue(selenium.isVisible(selection),
-            "The selection should be visible, since there is correct starting char!");
+        Actions builder = new Actions(webDriver);
+        builder.sendKeys(Keys.ENTER);
+        builder.build().perform();
 
-        selenium.keyPressNative(KeyEvent.VK_ENTER);
+        String actual = autocompleteSelectFirstFalse.getInputValue();
+        assertEquals(actual, expected, "The input should contains typed in value, not the suggestion!");
 
-        String actualValueOfInput = selenium.getValue(selectFirstFalseInput);
-        assertEquals(actualValueOfInput, "a",
-            "The content should be the letter which was types and not something else, since selectFirst is false!");
+        autocompleteSelectFirstFalse.type("l");
+        autocompleteSelectFirstFalse.autocompleteWithSuggestion(autocompleteSelectFirstFalse.getFirstSuggestion());
+
+        expected = "California";
+        actual = autocompleteSelectFirstFalse.getInputValue();
+        assertEquals(actual, expected, "The input should now contain value from suggestion");
     }
 
 }
