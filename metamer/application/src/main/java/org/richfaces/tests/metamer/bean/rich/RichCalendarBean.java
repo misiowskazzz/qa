802765c0550ae2088641d9c3ad079fcc58e227dd
Jiri Stefek <jstefek@redhat.com>
2013-02-25 15:50:28 +0100

    metamer: refactored validation (csv, jsr303): pages, beans, tests.

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/rich/RichCalendarBean.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/rich/RichCalendarBean.java
@@ -35,13 +35,11 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.validator.ValidatorException;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 
 import org.richfaces.component.UICalendar;
 import org.richfaces.tests.metamer.Attributes;
 import org.richfaces.tests.metamer.bean.RichBean;
+import org.richfaces.tests.metamer.bean.abstractions.DateInputValidationBeanImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,23 +47,17 @@ import org.slf4j.LoggerFactory;
  * Managed bean for rich:calendar.
  *
  * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
- * @version $Revision: 22820 $
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
  */
 @ManagedBean(name = "richCalendarBean")
 @ViewScoped
-public class RichCalendarBean implements Serializable {
+public class RichCalendarBean extends DateInputValidationBeanImpl implements Serializable {
 
     private static final long serialVersionUID = -1L;
     private static final Logger logger = LoggerFactory.getLogger(RichCalendarBean.class);
-    private Attributes attributes;
-    private Date date = new Date();
     public static final TimeZone TIME_ZONE = TimeZone.getTimeZone("UTC");
     private TimeZone tz = TimeZone.getTimeZone("GMT");
-    private Date date1;
-    private Date date2;
-    private Date date3;
     private Date currentDate;
-    private Date value;
 
     /**
      * Initializes the managed bean.
@@ -98,31 +90,6 @@ public class RichCalendarBean implements Serializable {
         attributes.remove("validator");
         attributes.remove("valueChangeListener");
         attributes.remove("timeZone");
-        attributes.remove("value");
-    }
-
-    public Attributes getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(Attributes attributes) {
-        this.attributes = attributes;
-    }
-
-    public Date getValue() {
-        return value;
-    }
-
-    public void setValue(Date value) {
-        this.value = value;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
     }
 
     public TimeZone getTimeZone() {
@@ -143,34 +110,6 @@ public class RichCalendarBean implements Serializable {
 
     public void setCurrentDate(Date currentDate) {
         this.currentDate = currentDate;
-    }
-
-    @Past
-    @NotNull
-    public Date getDate1() {
-        return date1;
-    }
-
-    public void setDate1(Date date1) {
-        this.date1 = date1;
-    }
-
-    @Future
-    @NotNull
-    public Date getDate2() {
-        return date2;
-    }
-
-    public void setDate2(Date date2) {
-        this.date2 = date2;
-    }
-
-    public Date getDate3() {
-        return date3;
-    }
-
-    public void setDate3(Date date3) {
-        this.date3 = date3;
     }
 
     /**
